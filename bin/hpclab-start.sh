fd13d7fe57a5e152a9a0c857d963fd814b4b65e8
David Cuthbert <dacut@kanga.org>
2016-11-06 23:57:22 -0800

    Make Flask listen externally.

--- bin/hpclab-start.sh
+++ bin/hpclab-start.sh
@@ -4,7 +4,7 @@ source /webapps/hpc-lab-maker/bin/activate
 test -d $VIRTUAL_ENV/run || mkdir -p $VIRTUAL_ENV/run
 test -d $VIRTUAL_ENV/log || mkdir -p $VIRTUAL_ENV/log
 cd $VIRTUAL_ENV
-FLASK_APP=hpclab.py $VIRTUAL_ENV/bin/flask run \
+FLASK_APP=hpclab.py $VIRTUAL_ENV/bin/flask run --host=0.0.0.0 \
   > $VIRTUAL_ENV/log/hpc-lab-maker.log 2>&1 &
 echo $! > $VIRTUAL_ENV/run/flask.pid
 disown %1
