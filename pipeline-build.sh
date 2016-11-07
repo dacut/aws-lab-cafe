338cd2ea8004d118ecda307cdda806e0b68f128b
David Cuthbert <dacut@kanga.org>
2016-11-06 22:41:34 -0800

    Fix pipeline-build.sh with renaming of scripts.

--- pipeline-build.sh
+++ pipeline-build.sh
@@ -4,6 +4,6 @@ virtualenv venv/pipeline
 source venv/pipeline/bin/activate
 python ./setup.py build
 python ./setup.py install
-cp bin/hpclab-start bin/hpclab-stop venv/pipeline/bin
+cp bin/hpclab-start.sh bin/hpclab-stop.sh venv/pipeline/bin
 cp appspec.yml venv/pipeline
 deactivate
