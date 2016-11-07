c97c2182eee031e72081c8cedb8ba8320fc2ddd8
David Cuthbert <dacut@kanga.org>
2016-11-06 23:37:38 -0800

    Locate requirements.txt in the deployment archive.

--- bin/hpclab-beforeinstall.sh
+++ bin/hpclab-beforeinstall.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+DEPLOYMENT_ARCHIVE=/opt/codedeploy-agent/deployment-root/${DEPLOYMENT_GROUP_ID}/${DEPLOYMENT_ID}/deployment-archive
 virtualenv /webapps/hpc-lab-maker
-echo "pwd before pip: $(pwd)"
-pip install --requirement requirements.txt --upgrade
+echo "Deployment archive: $DEPLOYMENT_ARCHIVE"
+pip install --requirement $DEPLOYMENT_ARCHIVE/requirements.txt --upgrade
