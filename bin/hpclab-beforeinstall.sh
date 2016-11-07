0057754aadb2c78e3d33cd0ed779a02fa3229620
David Cuthbert <dacut@kanga.org>
2016-11-06 23:43:00 -0800

    Fail if beforeinstall fails.

--- bin/hpclab-beforeinstall.sh
+++ bin/hpclab-beforeinstall.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+#!/bin/bash -e
 DEPLOYMENT_ARCHIVE=/opt/codedeploy-agent/deployment-root/${DEPLOYMENT_GROUP_ID}/${DEPLOYMENT_ID}/deployment-archive
 virtualenv /webapps/hpc-lab-maker
+source /webapps/hpc-lab-maker/bin/activate
 echo "Deployment archive: $DEPLOYMENT_ARCHIVE"
 pip install --requirement $DEPLOYMENT_ARCHIVE/requirements.txt --upgrade
