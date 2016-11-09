5e966c57afbe4f75a0fa95a1400bc4ecbd2ff3f3
David Cuthbert <dacut@kanga.org>
2016-11-09 14:20:07 -0800

    Build puttygen.

--- pipeline-build.sh
+++ pipeline-build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash -e
 mkdir pipeline
 cp -a bin static templates appspec.yml hpclab.py hpclab*.cfg requirements.txt pipeline
+
+cd putty-0.67
+./configure
+make -j 4
+cd ..
+cp putty-0.67/puttygen pipeline/bin/puttygen
