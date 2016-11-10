c56228fe3875e7e02dbfb042706f060d774cd2f1
David Cuthbert <dacut@kanga.org>
2016-11-10 14:27:41 -0800

    Install cracklib from yum repo.

--- bin/hpclab-beforeinstall.sh
+++ bin/hpclab-beforeinstall.sh
@@ -3,5 +3,6 @@ DEPLOYMENT_ARCHIVE=/opt/codedeploy-agent/deployment-root/${DEPLOYMENT_GROUP_ID}/
 virtualenv /webapps/hpc-lab-maker
 source /webapps/hpc-lab-maker/bin/activate
 echo "Deployment archive: $DEPLOYMENT_ARCHIVE"
-yum install -y gcc bintuils autoconf automake make libtool
+yum install -y gcc bintuils autoconf automake make libtool cracklib-devel \
+  cracklib-python cracklib-dicts
 pip install --requirement $DEPLOYMENT_ARCHIVE/requirements.txt --upgrade
