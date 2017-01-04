0581c0461ac7da2fc9abbe458d14bc9d4eaa2609
David Cuthbert <dacut@kanga.org>
2017-01-03 22:36:30 -0800

    Make this self-deploying (rather than via Zappa).

--- pipeline-build.sh
+++ pipeline-build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -ex
-mkdir pipeline
-cp -a bin static templates appspec.yml hpclab.py hpclab*.cfg requirements.txt pipeline
+python2.7 -m py_compile labcafe.py
+
+mkdir artifacts
+cp -a static templates labcafe.py defaults.cfg requirements.txt artifacts
 
 cd putty-0.67
 chmod ugo+x ./configure
 ./configure
 make -j 4
 cd ..
-cp putty-0.67/puttygen pipeline/bin/puttygen
+cp putty-0.67/puttygen artifacts/bin/puttygen
