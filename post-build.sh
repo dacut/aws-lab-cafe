d4dc0a1e26568481c5b9b83f8997f842eb9b974d
David Cuthbert <dacut@kanga.org>
2017-01-07 01:48:10 -0800

    Reorder cd commands.

--- post-build.sh
+++ post-build.sh
@@ -1,10 +1,8 @@
 #!/bin/bash -ex
 rm -f aws-lab-cafe.zip
 zip aws-lab-cafe.zip ./labcafe.pyc ./labcafe.py ./zappa_settings.json
-ls -l
-ls -l venv/lib/python2.7/site-packages
 pwd
 cd venv/lib/python2.7/site-packages
-pwd
-cd venv/lib/python2.7/site-packages && zip -u -r ../../../../aws-lab-cafe.zip .
+ls -l
+zip -u -r ../../../../aws-lab-cafe.zip .
 aws s3 cp aws-lab-cafe.zip s3://cuthbert-labcafe-artifacts
