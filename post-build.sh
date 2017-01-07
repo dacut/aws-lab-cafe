9d9b7c2edf90eb47f77fadd4bccc91c98b67cb36
David Cuthbert <dacut@kanga.org>
2017-01-07 03:32:00 -0800

    Always generate a unique zip filename. Add zappa to our build.

--- post-build.sh
+++ post-build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash -ex
-# Always start with a fresh Lambda bundle.
-rm -f aws-lab-cafe.zip
-
+TZ=UTC; export TZ
+DATETIME=$(date +'%Y%m%dT%H%M%SZ')
 # Add our app.
-zip -q aws-lab-cafe.zip ./labcafe.pyc ./labcafe.py ./zappa_settings.json
+zip -q aws-lab-cafe-${DATETIME}.zip ./labcafe.pyc ./labcafe.py ./zappa_settings.json
 
 
 # Add site-packages to the root of the Lambda bundle.
@@ -11,8 +10,12 @@ cd venv/lib/python2.7/site-packages
 
 # Copy cracklib libraries to the site-packages directory
 cp -p /usr/lib64/libcrack* .
-zip -q -u -r ../../../../aws-lab-cafe.zip .
+zip -q -u -r ../../../../aws-lab-cafe-${DATETIME}.zip .
 
 # Upload the Lambda bundle to S3.
 cd $CODEBUILD_SRC_DIR
-aws s3 cp aws-lab-cafe.zip s3://cuthbert-labcafe-artifacts
+aws s3 cp aws-lab-cafe-${DATETIME}.zip s3://cuthbert-labcafe-artifacts
+
+# Modify the CloudFormation script
+mv aws-lab-cafe.cfn aws-lab-cafe.cfn.in
+sed -e "s/@@DATETIME@@/${DATETIME}/g" aws-lab-cafe.cfn.in > aws-lab-cafe.cfn
