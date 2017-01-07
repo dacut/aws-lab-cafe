265a40d3e9a641413b652786ab98169262a821aa
David Cuthbert <dacut@kanga.org>
2017-01-07 02:01:58 -0800

    Make the zip operations quiet.

--- post-build.sh
+++ post-build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -ex
 rm -f aws-lab-cafe.zip
-zip aws-lab-cafe.zip ./labcafe.pyc ./labcafe.py ./zappa_settings.json
+zip -q aws-lab-cafe.zip ./labcafe.pyc ./labcafe.py ./zappa_settings.json
 pwd
 cd venv/lib/python2.7/site-packages
 ls -l
-zip -u -r ../../../../aws-lab-cafe.zip .
+zip -q -u -r ../../../../aws-lab-cafe.zip .
 cd $CODEBUILD_SRC_DIR
 aws s3 cp aws-lab-cafe.zip s3://cuthbert-labcafe-artifacts
