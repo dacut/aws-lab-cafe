c5d40b0c3fb4ca6d989180a47c210e117e80e730
David Cuthbert <dacut@kanga.org>
2017-01-07 02:50:36 -0800

    Add cracklib to the lambda bundle.

--- post-build.sh
+++ post-build.sh
@@ -1,9 +1,18 @@
 #!/bin/bash -ex
+# Always start with a fresh Lambda bundle.
 rm -f aws-lab-cafe.zip
+
+# Add our app.
 zip -q aws-lab-cafe.zip ./labcafe.pyc ./labcafe.py ./zappa_settings.json
-pwd
+
+
+# Add site-packages to the root of the Lambda bundle.
 cd venv/lib/python2.7/site-packages
-ls -l
+
+# Copy cracklib libraries to the site-packages directory
+cp -p /usr/lib64/libcrack* .
 zip -q -u -r ../../../../aws-lab-cafe.zip .
+
+# Upload the Lambda bundle to S3.
 cd $CODEBUILD_SRC_DIR
 aws s3 cp aws-lab-cafe.zip s3://cuthbert-labcafe-artifacts
