c845d3de4fcbead2bf945c7d89936f584f5f2cd8
David Cuthbert <dacut@kanga.org>
2017-01-11 08:29:32 +0000

    Add zappa_settings.py

---
+++ zappa_settings.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import, print_function
+from distutils.utils import strtobool
+from os import environ
+
+APP_MODULE = "labcafe"
+APP_FUNCTION = "app"
+EXECUTION_HANDLER = None
+DEBUG = strtobool(environ.get("DEBUG", "false"))
+LOG_LEVEL = ("DEBUG" if DEBUG else "INFO")
+DOMAIN = environ.get("DOMAIN")
+API_STAGE = "prod"
+PROJECT_NAME = "LabCafe"
