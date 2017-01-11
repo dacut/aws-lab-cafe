f3915f7484a36d3bee86e6418d26260358f21c02
David Cuthbert <dacut@kanga.org>
2017-01-11 14:07:22 +0000

    Add a few other settings that Zappa expects to be present.

--- zappa_settings.py
+++ zappa_settings.py
@@ -2,11 +2,15 @@ from __future__ import absolute_import, print_function
 from distutils.util import strtobool
 from os import environ
 
+API_STAGE = "prod"
 APP_MODULE = "labcafe"
 APP_FUNCTION = "app"
-EXECUTION_HANDLER = None
+AWS_EVENT_MAPPING = {}
 DEBUG = strtobool(environ.get("DEBUG", "false"))
-LOG_LEVEL = ("DEBUG" if DEBUG else "INFO")
+DJANGO_SETTINGS = None
 DOMAIN = environ.get("DOMAIN")
-API_STAGE = "prod"
+ENVIRONMENT_VARIABLES = {}
+EXCEPTION_HANDLER = None
+EXECUTION_HANDLER = None
+LOG_LEVEL = ("DEBUG" if DEBUG else "INFO")
 PROJECT_NAME = "LabCafe"
